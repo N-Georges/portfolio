1e66f3c3f262bb5a89887d2e5678d4eeba9410ff
Nodari Georges <gf.nodari@hotmail.com>
2022-08-19 03:44:38 +0200

    project design finish

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { About } from "../components/About";
 import { Banner } from "../components/Banner";
 import { Contact } from "../components/Contact";
 import { Footer } from "../components/Footer";
+import { Project } from "../components/Project";
 import { Services } from "../components/Services";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Banner />
       <About />
       <Services />
+      <Project />
       <Contact />
       <Footer />
     </div>
